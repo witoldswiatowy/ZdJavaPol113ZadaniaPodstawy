2c533f3a284eca4336818f23ebbc270afbec2d10
Witold Swiatowy <swiatowywitold@gmail.com>
2022-04-09 11:08:53 +0200

    solution day3_ex1

---
+++ src/Dzien9/Task1.java
@@ -0,0 +1,47 @@
+package Dzien9;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Task1 {
+
+//    private static final String EMAIL_VALIDATOR = "[A-Za-z0-9._-]+@[a-z]+(\\.[a-z]+)?";
+    private static final String EMAIL_VALIDATOR = "[\\w\\d._-]+@\\w+(\\.\\w+)?";
+
+    public static void main(String[] args) {
+        System.out.println(isCorrectFormat("sda@com."));
+
+
+    }
+    private static boolean isCorrectFormat (String emailAddress) {
+        /*
+        a? -> zero or one
+        a* -> zero or more
+        a+ -> one or more
+        \d -> dowolna cyfra
+        \w -> dowolna litera
+         */
+
+        /*
+            true:
+            xyz@com
+            xyz@com.pl
+            xyz_.-@com.pl
+            123xyz123@com.pl
+
+            false:
+            sad.com.pl
+            @sda
+            sda@
+            sda!@com.pl
+            @
+            sad@com.
+
+         */
+
+        Pattern pattern = Pattern.compile(EMAIL_VALIDATOR);
+//        Matcher matcher = pattern.matcher(emailAddress);
+//        return matcher.matches();
+        return pattern.matcher(emailAddress).matches();
+    }
+}
